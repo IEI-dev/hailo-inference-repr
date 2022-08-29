3f6eddbb3311f72cf1244bc3b0611631db9b9d31
Steven Wang 王先誠 <stevenwang@ieinet.org>
2022-08-29 17:33:02 +0800

    update test

--- src/react-player/components/WrapperRight/Points.test.js
+++ src/react-player/components/WrapperRight/Points.test.js
@@ -6,10 +6,6 @@ import "@testing-library/jest-dom/extend-expect";
 afterAll(cleanup);
 
 describe("render Points to test", () => {
-  const mockSetStartPoint = jest.fn();
-
-  const mockSetEndPoint = jest.fn();
-
   test("convert string to number", () => {
     expect(strToNum("3")).toEqual(3);
     expect(strToNum(3.14157)).toEqual(3);
@@ -22,36 +18,9 @@ describe("render Points to test", () => {
     expect(limitWidthHeight(15, 10)).toEqual(10);
   });
 
-  test("click reset btn trigger two function", () => {
-    render(
-      <Points
-        width={1280}
-        height={720}
-        startpoint={{ sx: 0, sy: 0 }}
-        endpoint={{ ex: 0, ey: 0 }}
-        setStartpoint={mockSetStartPoint}
-        setEndpoint={mockSetEndPoint}
-      />
-    );
-    const resetButton = screen.getByRole("button", { name: /Reset/i });
-    fireEvent.click(resetButton);
-    expect(mockSetStartPoint).toBeCalled();
-    expect(mockSetEndPoint).toBeCalled();
-  });
-  // test("click reset btn to reset sx, sy, ex, ey", () => {
-  //   render(
-  //     <Points
-  //       width={1280}
-  //       height={720}
-  //       startpoint={{ sx: 0, sy: 0 }}
-  //       endpoint={{ ex: 0, ey: 0 }}
-  //       setStartpoint={mockSetStartPoint}
-  //       setEndpoint={mockSetEndPoint}
-  //     />
-  //   );
+  // test("click reset btn trigger two function", () => {
+  //   render(<Points width={1280} height={720} />);
   //   const resetButton = screen.getByRole("button", { name: /Reset/i });
-  //   const sx = screen.getByRole('')
-  //   fireEvent.click(resetButton);
-  //   expect(sx.value).toBe(0);
+  //   expect(resetButton).toBeInTheDocument();
   // });
 });
