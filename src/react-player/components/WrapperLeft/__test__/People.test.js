b20a84158282124fbd251d95be42936fc38bb7b0
Steven Wang 王先誠 <stevenwang@ieinet.org>
2022-08-31 17:30:10 +0800

    update People.test.js

--- src/react-player/components/WrapperLeft/__test__/People.test.js
+++ src/react-player/components/WrapperLeft/__test__/People.test.js
@@ -1,17 +1,20 @@
 import React from "react";
 import People from "../People";
-import { render, screen } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import DataContextProvider from "../../../context/DataContext";
 import userEvent from "@testing-library/user-event";
 
-describe("test People.js component", () => {
-  it("correct inout information, current number and total number", () => {
+describe("test People.js component with frame set to 0", () => {
+  beforeEach(() => {
     render(
       <DataContextProvider>
         <People frame={0} />
       </DataContextProvider>
     );
+  });
+
+  it("correct inout information, current number and total number", () => {
     const inoutInfo = screen.getByText("in: 12 / out: 10");
     const countInfo = screen.getByText("num: 5 / all count: 5");
     expect(inoutInfo).toBeInTheDocument();
@@ -19,11 +22,6 @@ describe("test People.js component", () => {
   });
 
   it("Options length same as idAll's length, first frame attrs' length is 5", () => {
-    render(
-      <DataContextProvider>
-        <People frame={0} />
-      </DataContextProvider>
-    );
     const options = screen.getAllByRole("option");
     const firstFrameAttrs = screen.getAllByTestId("all_attrs");
     expect(options.length).toBe(35);
@@ -31,11 +29,6 @@ describe("test People.js component", () => {
   });
 
   it("select id 1, should have gender, age, objects, upper, lower attrs.", () => {
-    render(
-      <DataContextProvider>
-        <People frame={0} />
-      </DataContextProvider>
-    );
     const select = screen.getByRole("combobox", { name: /choose an id:/i });
     userEvent.selectOptions(select, "1");
     const gender = screen.getByText(/gender/i);
@@ -51,11 +44,6 @@ describe("test People.js component", () => {
   });
 
   it("select id 6, should show id not found text and instruction.", () => {
-    render(
-      <DataContextProvider>
-        <People frame={0} />
-      </DataContextProvider>
-    );
     const select = screen.getByRole("combobox", { name: /choose an id:/i });
     userEvent.selectOptions(select, "6");
     const notfoundText = screen.getByText("id 6 not found.");
@@ -63,13 +51,17 @@ describe("test People.js component", () => {
     expect(notfoundText).toBeInTheDocument();
     expect(instruction).toBeInTheDocument();
   });
+});
 
-  it("render with frame set to -1", () => {
+describe("test People.js component with frame set to -1", () => {
+  beforeEach(() => {
     render(
       <DataContextProvider>
         <People frame={-1} />
       </DataContextProvider>
     );
+  });
+  it("render with frame set to -1", () => {
     const pElement = screen.getByText(/nothing here/i);
     const h5Element = screen.getByRole("heading");
     expect(h5Element).toHaveTextContent("People Attributes");
