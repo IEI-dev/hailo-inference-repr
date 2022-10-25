eb86474a918d0f9e0bfa083820e8fe057bdbdeec
Steven Wang 王先誠 <stevenwang@ieinet.org>
2022-10-25 10:33:21 +0800

    delete redundant code

--- src/index.js
+++ src/index.js
@@ -14,58 +14,6 @@ import VideoContextProvider from "./react-player/context/VideoContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// scale
-document.addEventListener("keydown", function(event) {
-  const tagName = document.activeElement.tagName.toLowerCase();
-  if (tagName === "input") return;
-  const plus = document.querySelector(".plus");
-  const minus = document.querySelector(".minus");
-  // const play = document.querySelector("#play");
-  const play = document.querySelector(".play-pause-btn");
-  const mute = document.querySelector("#mute");
-  // const fullscreen = document.querySelector(".fullscreen");
-  const fullscreen = document.querySelector(".full-screen-btn");
-  const volumeUp = document.querySelector(".volumeUp");
-  const volumeDown = document.querySelector(".volumeDown");
-  const rewind = document.querySelector(".rewind");
-  const forward = document.querySelector(".forward");
-  // eslint-disable-next-line
-  switch (event.key.toLowerCase()) {
-    case " ":
-      if (tagName === "button") return;
-      play.click();
-      break;
-    case "=":
-    case "+":
-      plus.click();
-      break;
-    case "-":
-      minus.click();
-      break;
-    case "f":
-      fullscreen.click();
-      break;
-    case "m":
-      mute.click();
-      break;
-  }
-  // eslint-disable-next-line
-  switch (event.key) {
-    case "ArrowLeft":
-      rewind.click();
-      break;
-    case "ArrowRight":
-      forward.click();
-      break;
-    case "ArrowUp":
-      volumeUp.click();
-      break;
-    case "ArrowDown":
-      volumeDown.click();
-      break;
-  }
-});
-
 root.render(
   <BrowserRouter>
     {/* <Navbar /> */}
