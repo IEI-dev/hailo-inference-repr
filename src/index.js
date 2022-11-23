3c6452be2479118c288b68ec3a532bf03b3e6e4f
Steven Wang 王先誠 <stevenwang@ieinet.org>
2022-11-23 17:59:10 +0800

    remove redundants

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Navbar from "./Navbar";
-import CanvasPractices from "./canvas/index";
-import Websocket from "./websocket/Websocket";
-import BtsSocket from "./websocket/BtsSocket";
-import Chat from "./websocket/Chat";
 import "./css/style.css";
 import DataContextProvider from "./react-player/context/DataContext";
 import CanvasContextProvider from "./react-player/context/CanvasContext";
@@ -30,10 +25,6 @@ root.render(
           </VideoContextProvider>
         }
       />
-      <Route path="/websocket" element={<Websocket />} />
-      <Route path="/canvas" element={<CanvasPractices />} />
-      <Route path="/websocket/btssocket" element={<BtsSocket />} />
-      <Route path="/websocket/chat" element={<Chat />} />
     </Routes>
   </BrowserRouter>
 );
